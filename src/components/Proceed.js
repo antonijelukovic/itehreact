8cd04bd0e74cfd1ba8ca61e14a676f0c57951bc8
antonijelukovic <antonijelukovic3@gmail.com>
2022-02-05 22:49:31 +0100

    Proceed.js, Link do /join path, komponenta Join.js, u Routeru definisana ruta /join

--- src/components/Proceed.js
+++ src/components/Proceed.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+import { Link } from "react-router-dom";
 
 
 function Proceed(props) {
 
+    const [name, setName] = useState('');
+
+    function handleName(e) {
+        setName(e.target.value);
+    }
 
     return (
         <div className="pocetni_div">
             <h1 id="main_naslov">{props.mainNaslov}</h1>
+            <input type="text" id="input-proceed" className="form-control" value={name} onChange={handleName} placeholder="Enter your name to proceed..." />
+
+            <Link to='/join' state={{ name: name }} >
+                <button type="button" id='button-proceed' className='btn btn-danger mt-3'>Proceed</button>
+            </Link>
+
         </div>
     );
 }
